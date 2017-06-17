e03d955a8be536a56259d0c222f982ee0ccd2c90
David J. H. Shih <djh.shih@gmail.com>
2017-06-17 18:35:22 -0400

    Change survey question. Save results under surveys key.

--- web/scripts/survey.js
+++ web/scripts/survey.js
@@ -2,18 +2,26 @@
 
 Survey.Survey.cssType = "bootstrap";
 
-var surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
-    { name:"page1", questions: [ 
-            { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
-                  { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visibleIf: "{frameworkUsing} = 'Yes'" }
-               ]},
-      { name: "page2", questions: [
-            { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
-                { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" } ] },
+var surveyJSON = {
+    title: "Please tell us about yourself.",
+    pages: [
+        { name: "page0", questions: [
+            { type: "comment", name: "name", title: "What is your full name?" }
+        ] },
+        { name: "page1", questions: [ 
+            { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "forSelf", title: "Are you filling out this survey for yourself?" },
+            { type: "radiogroup", choices: ["Yes", "No"], isRequired: true, name: "isAdult", title: "Are you 18 years or older?" },
+            { type: "radiogroup", choices: [ "Male", "Female" ], isRequired: true, name: "sex", title: "What is your biological sex?", visibleIf: "{isAdult} = 'Yes'" },
+        ] },
+        { name: "page2", questions: [
+            { type: "radiogroup", choices: ["Public", "Private"], hasOther: true, isRequired: false, name: "healthCareProvider", title: "Who is your health care service provider?" },
+            { type: "comment", isRequired: true, name: "primaryPhysician", title: "Who is your primary attending physician?"}
+        ] },
         { name: "page3",questions: [
-              { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
-     ]
-}
+            { type: "comment", name: "about", title: "Please tell us about anything else you feel we should know." }
+        ] }
+    ]
+};
 
 var survey = new Survey.Model(surveyJSON);
 $("#surveyContainer").Survey({
@@ -22,13 +30,16 @@ $("#surveyContainer").Survey({
 });
 
 function sendDataToServer(survey) {
-    var resultAsString = JSON.stringify(survey.data);
+    //var resultAsString = JSON.stringify(survey.data);
+    var database = firebase.database();
 
     // Push result to firebase database
-    firebase.database().ref('messages').push({
+    database.ref('messages').push({
         name: 'Data Bee',
         photoUrl: '/images/bee-logo.jpg',
-        text: `Survey result saved: ${resultAsString}`
+        text: `Thank you for filling out the survey, ${survey.data.name}.`
     });
+
+    database.ref('surveys').push(survey.data);
 }
 
